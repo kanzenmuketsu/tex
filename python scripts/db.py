b3ea88bb7ec566c00db0104d8d093d622f8c7864
mentality champion <maksedetnati@gmail.com>
2024-11-22 09:17:14 +0700

    to server

--- python scripts/db.py
+++ python scripts/db.py
@@ -51,6 +51,7 @@ def insert_one(dictionary_data: dict): #username: username
 def get_product_from_db_by_name(page_name):
     connect_db()
     page_name = page_name.split('.')[0].replace('-', ' ')
+    print(page_name,'#######')
     query = f'SELECT * FROM {db_name}.products WHERE products.short_info = \'{page_name}\''
     with connection.cursor() as cursor:
         cursor.execute(query)
