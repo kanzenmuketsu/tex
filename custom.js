70f434e29b3b5151992b42332e508bc830e9987a
mentality champion <maksedetnati@gmail.com>
2024-11-22 09:45:20 +0700

    to server

--- custom.js
+++ custom.js
@@ -127,3 +127,17 @@ async function buy(){
         alert('ok')
     }
 }
+
+const popupOverlay = document.getElementById("popup-overlay");
+const popup = document.getElementById("popup");
+
+function showPopup() {
+  popupOverlay.style.display = "block";
+}
+
+function hidePopup() {
+  popupOverlay.style.display = "none";
+}
+
+popupOverlay.addEventListener("click", hidePopup);
+popup.addEventListener("click", (event) =&gt; event.stopPropagation());
