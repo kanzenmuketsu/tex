a50493ce66f621275c310af7fb4ad80ccbefb1f7
mentality champion <maksedetnati@gmail.com>
2024-11-24 10:01:17 +0700

    to server

--- python scripts/db.py
+++ python scripts/db.py
@@ -40,6 +40,7 @@ def check_exist(username):
 def insert_order(dictionary_data: dict):
     connect_db()
     values = list(dictionary_data.values())
+    values[2] = get_user_from_db(values[3])[0]
     query = f'INSERT INTO {db_name}.order (order_id, product_id, user_id, count) VALUES (\'{values[0]}\', \'{values[1]}\', \'{values[2]}\', \'{values[3]}\')'
     print(query)
     try:
